a4485b91970ed0f4c450ff141f2c98bc82ea63fd
Kwounsu <sb00lgs@gmail.com>
2020-06-30 09:35:48 -0700

    Camera

--- app/src/main/java/com/example/media/CameraActivity.kt
+++ app/src/main/java/com/example/media/CameraActivity.kt
@@ -12,10 +12,12 @@ import kotlinx.android.synthetic.main.activity_camera.*
 class CameraActivity : AppCompatActivity() {
     private val CAMERA_REQUEST = 1888
     @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
+        // Take a picture
         button_camera.setOnClickListener {
             val cameraIntent = Intent(ACTION_IMAGE_CAPTURE_SECURE)
             startActivityForResult(cameraIntent, CAMERA_REQUEST)
@@ -24,9 +26,13 @@ class CameraActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode != RESULT_CANCELED) {
+
+        // Change ImageView to the photo took by camera.
+        if(resultCode != RESULT_CANCELED && requestCode == CAMERA_REQUEST) {
             val photo = data?.extras!!["data"] as Bitmap
             imageView?.setImageBitmap(photo)
+        } else {
+            imageView?.setImageResource(R.mipmap.ic_launcher)
         }
     }
 }
